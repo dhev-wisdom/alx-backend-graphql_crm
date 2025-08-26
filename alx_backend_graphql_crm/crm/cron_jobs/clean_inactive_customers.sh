089433c404639f97ebbb17b5c9c92a864e4c66d5
Chinonso Wisdom <nonsowisdom62@gmail.com>
2025-08-26 16:55:38 +0100

    cont jobs

---
+++ alx_backend_graphql_crm/crm/cron_jobs/clean_inactive_customers.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+PROJECT_DIR="C:\\Users\\nonso\\Desktop\\ALX_PRODEV_BACKEND\\Alx-GraphQL\\alx_backend_graphql_crm"
+MANAGE_PY="$PROJECT_DIR/manage.py"
+
+LOG_FILE="/tmp/customer_cleanup_log.txt"
+
+DELETED_COUNT=$(python $MANAGE_PY shell -c "
+from django.utils import timezone
+from datetime import timedelta
+from crm.models import Customer
+
+one_year_ago = timezone.now() - timedelta(days=365)
+inactive_customers = Customer.objects.filter(orders__isnull=True) | Customer.objects.exclude(orders__order_date__gte=one_year_ago)
+
+inactive_customers = inactive_customers.distinct()
+
+count = inactive_customers.count()
+
+inactive_customers.delete()
+print(count)
+")
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Deleted $DELETED_COUNT inactive customers" >> "$LOG_FILE"
